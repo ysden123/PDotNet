f93508c749d601b3857c5a5ef758a861acc2b581
Yuriy Stul <ysden123@gmail.com>
2024-05-13 10:32:17 +0300

    Menu and multiple windows

---
+++ WpfAppMW/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfAppMW
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ WpfAppMW/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace WpfAppMW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is the action 1");
+        }
+
+        private void MenuItem_Click2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is the action 2");
+
+        }
+    }
+}
