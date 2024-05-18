ac05beff2d875529462f592ce038a65446d66262
Yuriy Stul <ysden123@gmail.com>
2024-05-18 17:54:51 +0300

    Check if the Debug mode is active.

---
+++ BasicTests/PDebug/CheckDebugTests.cs
@@ -0,0 +1,20 @@
+﻿
+namespace StulSoft.BasicTests.PDebug
+{
+    internal class CheckDebugTests
+    {
+        static readonly bool _IsDebug = System.Diagnostics.Debugger.IsAttached;
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("==>CheckDebugTests.Test1");
+            Console.WriteLine(_IsDebug ? "Is attached." : "Is not attcahed");
+        }
+    }
+}
