1714ef0a05d5e43e9eca96cd494f5f204ae91b25
Yuriy Stul <ysden123@gmail.com>
2024-05-26 09:05:38 +0300

    Directory list

---
+++ PIO/DirList.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+
+namespace PIO
+{
+    internal class DirList
+    {
+        public DirList() { }
+
+        private DirectoryInfo? DefineParentDirInfo(DirectoryInfo? di)
+        {
+            if (di != null)
+            {
+                return Directory.GetParent(di.FullName);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void ListDirs()
+        {
+            Console.WriteLine("==>DirList.ListDirs");
+            var enumerationOptions = new EnumerationOptions();
+            enumerationOptions.RecurseSubdirectories = true;
+            string currentDirectory = Directory.GetCurrentDirectory();
+            Console.WriteLine($"Current directory is : {currentDirectory}");
+
+            var parentDir = Directory.GetParent(currentDirectory);
+            parentDir = DefineParentDirInfo(parentDir);
+            parentDir = DefineParentDirInfo(parentDir);
+            parentDir = DefineParentDirInfo(parentDir);
+
+            string? parent = (parentDir == null) ? null : parentDir.FullName;
+            if (parent != null)
+            {
+                Console.WriteLine($"Parent directory is  : {parent}");
+                var directories = Directory.EnumerateDirectories(parent, "*", enumerationOptions);
+                foreach (var directory in directories)
+                {
+                    Console.WriteLine($"{directory}");
+                }
+
+                Console.WriteLine("List empty directories.");
+                foreach (var directory in directories)
+                {
+                    if (!Directory.EnumerateFileSystemEntries(directory).Any())
+                    {
+                        Console.WriteLine($"Empty directory: {directory}");
+                    }
+                }
+            }
+        }
+
+        public void ListEmptyDirs(string parent)
+        {
+            Console.WriteLine($"==>DirList.ListEmptyDirs ({parent})");
+
+            var enumerationOptions = new EnumerationOptions();
+            enumerationOptions.RecurseSubdirectories = true;
+
+            var directories = Directory.EnumerateDirectories(parent, "*", enumerationOptions);
+
+            Console.WriteLine($"List empty directories. Total number of directories is {directories.Count()}");
+            foreach (var directory in directories)
+            {
+                if (!Directory.EnumerateFileSystemEntries(directory).Any())
+                {
+                    Console.WriteLine($"Empty directory: {directory}");
+                }
+            }
+        }
+    }
+}
---
+++ PIO/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace PIO
+{
+    internal class Program()
+    {
+        public static void Main()
+        {
+            Console.WriteLine("==>Program.Main");
+
+            var dl=new DirList();
+            dl.ListDirs();
+
+            dl.ListEmptyDirs(@"d:\Photo\YSPhoto\Images\");
+        }
+    }
+}
