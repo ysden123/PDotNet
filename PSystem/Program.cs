eb96d167a4405048a0d53119be1c403cb20e6ad7
Yuriy Stul <ysden123@gmail.com>
2024-07-14 11:31:22 +0300

    Show env variables

---
+++ PSystem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace PSystem
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            ShowEnvVariables();
+        }
+
+        private static void ShowEnvVariables()
+        {
+            Console.WriteLine();
+            Console.WriteLine("==>ShowEnvVariables");
+
+            var dic = new SortedDictionary<string, string?>();
+
+            foreach (DictionaryEntry item in Environment.GetEnvironmentVariables())
+            {
+                string? key = item.Key?.ToString();
+                string? value = item.Value?.ToString();
+
+                if (key != null)
+                    dic.Add(key, value);
+            }
+
+            foreach (var item in dic)
+            {
+                Console.WriteLine("  {0} = {1}", item.Key, item.Value);
+            }
+        }
+    }
+}
