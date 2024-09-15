18d8ebf1947b3e0ac89651a5d5766a63f74a756f
Yuriy Stul <ysden123@gmail.com>
2024-09-15 10:20:13 +0300

    New tests

---
+++ BasicTests/PFormatting/NumberFormatting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StulSoft.BasicTests.PFormatting
+{
+    internal class NumberFormatting
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void FormatNumber()
+        {
+            int i = 1;
+
+            string f = $"i={i}";
+            Console.WriteLine(f);
+
+            f = $"i={i:D2}";
+            Console.WriteLine(f);
+        }
+    }
+}
