34e78ce148afec5df51eff1b4a79700c7919ee50
Yuriy Stul <ysden123@gmail.com>
2024-10-25 11:11:37 +0300

    Timers

---
+++ PTime/PTimer01.cs
@@ -0,0 +1,43 @@
+﻿using System.Timers;
+
+namespace PTime
+{
+    internal class PTimer01
+    {
+        public static void SimpleTimer()
+        {
+            Console.WriteLine("==>SimpleTimer");
+            System.Timers.Timer timer = new(1500); // In 1.5 sec
+            timer.Elapsed += (sender, e) =>
+            {
+                Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+                              e.SignalTime);
+            };
+            timer.Start();
+
+            Console.WriteLine("Press any key to stop... ");
+            Console.ReadKey();
+            timer.Stop();
+            timer.Dispose();
+
+        }
+        public static void TimerWithReset()
+        {
+            Console.WriteLine("==>TimerWithReset");
+            System.Timers.Timer timer = new(1500); // In 1.5 sec
+            timer.Elapsed += (sender, e) =>
+            {
+                Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+                              e.SignalTime);
+                timer.Enabled = false;
+            };
+            timer.Start();
+
+            Console.WriteLine("Press any key to stop... ");
+            Console.ReadKey();
+            timer.Stop();
+            timer.Dispose();
+
+        }
+    }
+}
---
+++ PTime/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace PTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PTime");
+
+            PTimer01.SimpleTimer();
+
+            PTimer01.TimerWithReset();
+        }
+    }
+}
