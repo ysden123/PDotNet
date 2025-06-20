d071d5e74a8d63f429f00e95eed945fb4ceea60d
Yuriy Stul <ysden123@gmail.com>
2025-06-20 19:57:10 +0300

    MVVM and Data Binding - 1st steps

---
+++ MVVMDemo/ViewModel/StudentViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using static MVVMDemo.Model.StudentModel;
+
+namespace MVVMDemo.ViewModel
+{
+    internal class StudentViewModel
+    {
+        public ObservableCollection<Student>? Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students =
+            [
+                new Student { FirstName = "Mark", LastName = "Allain" },
+                new Student { FirstName = "Allen", LastName = "Brown" },
+                new Student { FirstName = "Linda", LastName = "Hamerski" },
+            ];
+
+            Students = students;
+        }
+    }
+}
