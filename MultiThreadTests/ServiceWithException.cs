496a195858a36915991dd26e6aadad75565f9ad7
Yuriy Stul <ysden123@gmail.com>
2025-11-18 13:21:07 +0200

    Multiple threads with exception

---
+++ MultiThreadTests/ServiceWithException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreadTests
+{
+    internal static class ServiceWithException
+    {
+        internal static async Task<string> Method1(bool throwException)
+        {
+            try {
+                string? result = await Task<string>.Run(async () =>
+                {
+                    if (throwException)
+                        throw new InvalidOperationException("An error occurred in Method1's task.");
+                    return "Hello, World!";
+                    
+                });
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Method1: exception - {ex}");
+                throw;
+            }
+        }
+    }
+}
---
+++ MultiThreadTests/ServiceWithExceptionTests.cs
@@ -0,0 +1,109 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MultiThreadTests;
+
+public class ServiceWithExceptionTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task TestOnceMethod1()
+    {
+        try
+        {
+            string? result = await ServiceWithException.Method1(true);
+            Assert.That(result, Is.Null);
+        }
+        catch (Exception ex)
+        {
+            Assert.That(ex, Is.TypeOf<InvalidOperationException>());
+            Assert.That(ex.Message, Is.EqualTo("An error occurred in Method1's task."));
+        }
+    }
+
+    [Test]
+    public async Task TestSeveralTimesMethod1AllFail()
+    {
+        try
+        {
+            var taskList = new List<Task<string>>()
+            {
+                ServiceWithException.Method1(true),
+                ServiceWithException.Method1(true)
+            };
+
+            string[]? result = await Task.WhenAll(taskList);
+            Assert.Fail("Exception must be thrown!");
+        }
+        catch (Exception ex)
+        {
+            Assert.That(ex, Is.TypeOf<InvalidOperationException>());
+            Assert.That(ex.Message, Is.EqualTo("An error occurred in Method1's task."));
+        }
+    }
+
+    [Test]
+    public async Task TestSeveralTimesMethod1AllSucceed()
+    {
+        try
+        {
+            var taskList = new List<Task<string>>()
+            {
+                ServiceWithException.Method1(false),
+                ServiceWithException.Method1(false)
+            };
+
+            string[]? result = await Task.WhenAll(taskList);
+            Assert.That(result, Is.Not.Null);
+        }
+        catch (Exception ex)
+        {
+            Assert.Fail("No exception must be thrown!");
+        }
+    }
+
+    [Test]
+    public async Task TestSeveralTimesMethod1FirstFail()
+    {
+        try
+        {
+            var taskList = new List<Task<string>>()
+            {
+                ServiceWithException.Method1(true),
+                ServiceWithException.Method1(false)
+            };
+
+            string[]? result = await Task.WhenAll(taskList);
+            Assert.Fail("Exception must be thrown!");
+        }
+        catch (Exception ex)
+        {
+            Assert.That(ex, Is.TypeOf<InvalidOperationException>());
+            Assert.That(ex.Message, Is.EqualTo("An error occurred in Method1's task."));
+        }
+    }
+
+    [Test]
+    public async Task TestSeveralTimesMethod1SecondFail()
+    {
+        try
+        {
+            var taskList = new List<Task<string>>()
+            {
+                ServiceWithException.Method1(false),
+                ServiceWithException.Method1(true)
+            };
+
+            string[]? result = await Task.WhenAll(taskList);
+            Assert.Fail("Exception must be thrown!");
+        }
+        catch (Exception ex)
+        {
+            Assert.That(ex, Is.TypeOf<InvalidOperationException>());
+            Assert.That(ex.Message, Is.EqualTo("An error occurred in Method1's task."));
+        }
+    }
+}
