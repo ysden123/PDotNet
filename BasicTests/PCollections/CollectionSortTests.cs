2e5db6b10f1661fbe2a7083fbc7d4caf1d7092ec
Yuriy Stul <ysden123@gmail.com>
2025-11-19 14:22:08 +0200

    Sorting with LINQ

--- BasicTests/PCollections/CollectionSortTests.cs
+++ BasicTests/PCollections/CollectionSortTests.cs
@@ -1,6 +1,5 @@
 ﻿namespace StulSoft.BasicTests;
 
-
 public class CollectionSortTests
 {
     record Object4Sort
@@ -17,34 +16,124 @@ public class CollectionSortTests
     [Test]
     public void SortOrdinalTest()
     {
-        List<Object4Sort> list = 
+        List<Object4Sort> list =
         [
             new Object4Sort() { Name = "name3", Age = 30 },
             new Object4Sort() { Name = "name1", Age = 10 },
             new Object4Sort() { Name = "Name2", Age = 20 }
         ];
 
-        list.Sort((a,b) => string.Compare(a.Name, b.Name, StringComparison.Ordinal));
+        list.Sort((a, b) => string.Compare(a.Name, b.Name, StringComparison.Ordinal));
 
-        Assert.That(list[0].Name, Is.EqualTo("Name2"));
-        Assert.That(list[1].Name, Is.EqualTo("name1"));
-        Assert.That(list[2].Name, Is.EqualTo("name3"));
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(list[0].Name, Is.EqualTo("Name2"));
+            Assert.That(list[1].Name, Is.EqualTo("name1"));
+            Assert.That(list[2].Name, Is.EqualTo("name3"));
+        }
+
+        List<Object4Sort> list2 =
+        [
+            new Object4Sort() { Name = "name3", Age = 30 },
+            new Object4Sort() { Name = "name1", Age = 10 },
+            new Object4Sort() { Name = "Name2", Age = 20 }
+        ];
+
+        list2.Sort((a, b) => string.CompareOrdinal(a.Name, b.Name));
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(list2[0].Name, Is.EqualTo("Name2"));
+            Assert.That(list2[1].Name, Is.EqualTo("name1"));
+            Assert.That(list2[2].Name, Is.EqualTo("name3"));
+        }
     }
 
     [Test]
     public void SortOrdinalIgnoreCaseTest()
     {
-        List<Object4Sort> list = 
+        List<Object4Sort> list =
         [
             new Object4Sort() { Name = "name3", Age = 30 },
             new Object4Sort() { Name = "name1", Age = 10 },
             new Object4Sort() { Name = "Name2", Age = 20 }
         ];
 
-        list.Sort((a,b) => string.Compare(a.Name, b.Name, StringComparison.OrdinalIgnoreCase));
+        list.Sort((a, b) => string.Compare(a.Name, b.Name, StringComparison.OrdinalIgnoreCase));
 
-        Assert.That(list[0].Name, Is.EqualTo("name1"));
-        Assert.That(list[1].Name, Is.EqualTo("Name2"));
-        Assert.That(list[2].Name, Is.EqualTo("name3"));
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(list[0].Name, Is.EqualTo("name1"));
+            Assert.That(list[1].Name, Is.EqualTo("Name2"));
+            Assert.That(list[2].Name, Is.EqualTo("name3"));
+        }
+    }
+
+    [Test]
+    public void SortWithLINQTest()
+    {
+        List<Object4Sort> list =
+        [
+            new Object4Sort() { Name = "name3", Age = 30 },
+            new Object4Sort() { Name = "name1", Age = 10 },
+            new Object4Sort() { Name = "Name2", Age = 20 }
+        ];
+
+        var query = from item in list
+                    orderby item.Name
+                    select item;
+        var sortedList = query.ToList();
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(sortedList[0].Name, Is.EqualTo("name1"));
+            Assert.That(sortedList[1].Name, Is.EqualTo("Name2"));
+            Assert.That(sortedList[2].Name, Is.EqualTo("name3"));
+        }
+    }
+
+    [Test]
+    public void SortWithLINQ2Test()
+    {
+        List<Object4Sort> list =
+        [
+            new Object4Sort() { Name = "name3", Age = 30 },
+            new Object4Sort() { Name = "name1", Age = 10 },
+            new Object4Sort() { Name = "Name2", Age = 20 }
+        ];
+
+        var sortedList = (from item in list
+                          orderby item.Name
+                          select item).ToList();
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(sortedList[0].Name, Is.EqualTo("name1"));
+            Assert.That(sortedList[1].Name, Is.EqualTo("Name2"));
+            Assert.That(sortedList[2].Name, Is.EqualTo("name3"));
+        }
+    }
+
+    [Test]
+    public void SortWithLINQ3Test()
+    {
+        List<Object4Sort> list =
+        [
+            new Object4Sort() { Name = "name3", Age = 30 },
+            new Object4Sort() { Name = "name1", Age = 10 },
+            new Object4Sort() { Name = "Name2", Age = 20 }
+        ];
+
+        var sortedList = (from item in list
+                          orderby item.Name
+                          descending
+                          select item).ToList();
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(sortedList[0].Name, Is.EqualTo("name3"));
+            Assert.That(sortedList[1].Name, Is.EqualTo("Name2"));
+            Assert.That(sortedList[2].Name, Is.EqualTo("name1"));
+        }
     }
 }
